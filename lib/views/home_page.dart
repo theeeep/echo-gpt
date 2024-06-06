6dcb26a81931fd072fdb018afd6eea9f164168c5
Deepak <ideepakrabindra@gmail.com>
2024-06-07 04:58:18 +0530

    ui fix

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -9,6 +9,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _controller = TextEditingController();
   final List<Message> _message = [
     Message(text: "Hi", isUser: true),
     Message(text: "Hello, what's up ?", isUser: false),
@@ -19,40 +20,71 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: _message.length,
-        itemBuilder: (context, index) {
-          final message = _message[index];
-          return ListTile(
-            title: Align(
-              alignment:
-                  message.isUser ? Alignment.centerRight : Alignment.centerLeft,
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: message.isUser ? Colors.green : Colors.grey[300],
-                  borderRadius: message.isUser
-                      ? const BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          bottomRight: Radius.circular(20),
-                          bottomLeft: Radius.circular(20),
-                        )
-                      : const BorderRadius.only(
-                          topRight: Radius.circular(20),
-                          bottomRight: Radius.circular(20),
-                          bottomLeft: Radius.circular(20),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _message.length,
+              itemBuilder: (context, index) {
+                final message = _message[index];
+                return ListTile(
+                  title: Align(
+                    alignment: message.isUser
+                        ? Alignment.centerRight
+                        : Alignment.centerLeft,
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: message.isUser ? Colors.green : Colors.grey[300],
+                        borderRadius: message.isUser
+                            ? const BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                bottomRight: Radius.circular(20),
+                                bottomLeft: Radius.circular(20),
+                              )
+                            : const BorderRadius.only(
+                                topRight: Radius.circular(20),
+                                bottomRight: Radius.circular(20),
+                                bottomLeft: Radius.circular(20),
+                              ),
+                      ),
+                      child: Text(
+                        message.text,
+                        style: TextStyle(
+                          color: message.isUser ? Colors.white : Colors.black,
                         ),
-                ),
-                child: Text(
-                  message.text,
-                  style: TextStyle(
-                    color: message.isUser ? Colors.white : Colors.black,
+                      ),
+                    ),
                   ),
-                ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _controller,
+                      decoration: const InputDecoration(
+                        hintText: 'Ask anything',
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.send_rounded),
+                  ),
+                ],
               ),
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
